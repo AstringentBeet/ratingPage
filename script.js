c4a748ec4e772db71c516aaaf9bddfaa9a7a85e4
Alexander Effanga <alexandereffanga@alexanders-air.attlocal.net>
2022-05-23 19:27:36 -0500

    going to rewrite large portions of html and CSS; pseudo-elements and overly documented labels aren't cutting for Javascript

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const button = document.querySelector("li");
+let numbers = document.querySelector(".rate--number");
+let submit = document.getElementById("button");
+let survey = document.querySelector(".survey");
+let userRating = 0;
+let score = document.querySelector(".thanks--outOf");
+let thxPage = document.querySelector(".thanks")
 
+
+
+submit.addEventListener('click', e => {
+    setTimeout(function () {
+        survey.className = "hidden";
+        thxPage.classList.remove("hidden");
+        thxPage.style.animation = "fadein 1.5s ease-out forwards";
+    }, 250)
+});
