0fc44d2a61062b330a99e5787e4cbf1cb650244e
Alexander Effanga <alexandereffanga@alexanders-air.attlocal.net>
2022-05-25 15:15:29 -0500

    was finally able to figure it out; thank the stars for classList.toggle

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-let numbers = document.querySelector(".rate--number");
 let submit = document.getElementById("button");
 let survey = document.querySelector(".survey");
-let userRating = 0;
 let score = document.querySelector("#thanks--outOf");
 let thxPage = document.querySelector(".thanks")
 
-
-
 submit.addEventListener('click', e => {
-    let userSelection = document.querySelector("input[type='radio'][name='rate']:checked").value;
+    let radio = document.querySelectorAll("input[name='rate']");
+    let userSelection;
     e.preventDefault();
-    if(userSelection != 0){
-        setTimeout(function () {
-            score.innerHTML = userSelection;
-            survey.className = "hidden";
-            thxPage.classList.remove("hidden");
-            thxPage.style.animation = "fadein 1.5s ease-out forwards";
-        }, 250);
-    } 
+    radio.forEach(radioButton => {
+        if(radioButton.checked){
+            userSelection = radioButton.value;
+            setTimeout(function () {
+                score.innerHTML = userSelection;
+                survey.className = "hidden";
+                thxPage.classList.remove("hidden");
+                thxPage.style.animation = "fadein 1.5s ease-out forwards";
+            }, 250);
+        } else if(radioButton.checked === false) {
+            function error (){
+                document.querySelector(".submit").className = "submit";
+                window.requestAnimationFrame(function(time){
+                    window.requestAnimationFrame(function(time){
+                        document.querySelector(".submit").classList.toggle("error")
+                    })
+                })
+            }
+            error();
+        }
+    });
 });
